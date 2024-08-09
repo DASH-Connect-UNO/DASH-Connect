215b6794df73c8fd61431bf4fe16a2e0831d5db8
csensenig1 <111775844+csensenig1@users.noreply.github.com>
2024-08-09 00:21:34 -0500

    merged from main

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from .models import StudentProfile, AdminProfile, CustomUser
+from .models import StudentProfile, AdminProfile, CustomUser, VisitReason
+from django.utils.translation import gettext_lazy as _
 
 # Get the user model
 User = get_user_model()
@@ -33,3 +34,39 @@ class DeactivateAdminForm(forms.Form):
 # Form for reactivating an admin
 class ReactivateAdminForm(forms.Form):
     admin_id = forms.IntegerField()
+
+class VisitReasonForm(forms.ModelForm):
+    class Meta:
+        model = VisitReason
+        fields = ['appointment', 'printing', 'study', 'socialize', 'event', 'schedule_appointment', 'hardship',
+                  'basic_needs_support', 'financial_wellness', 'volunteer_opportunities']
+        labels = {
+            'appointment': _('Appointment with DASH staff'),
+            'printing': _('Printing'),
+            'study': _('Study'),
+            'socialize': _('Socialize/Relax'),
+            'event': _('Event'),
+            'schedule_appointment': _('Schedule an appointment'),
+            'hardship': _('Hardship'),
+            'basic_needs_support': _('Basic Needs Support'),
+            'financial_wellness': _('Financial Wellness'),
+            'volunteer_opportunities': _('Volunteer Opportunities'),
+        }
+
+        widgets = {
+            'appointment': forms.CheckboxInput(),
+            'printing': forms.CheckboxInput(),
+            'study': forms.CheckboxInput(),
+            'socialize': forms.CheckboxInput(),
+            'event': forms.CheckboxInput(),
+            'schedule_appointment': forms.CheckboxInput(),
+            'hardship': forms.CheckboxInput(),
+            'basic_needs_support': forms.CheckboxInput(),
+            'financial_wellness': forms.CheckboxInput(),
+            'volunteer_opportunities': forms.CheckboxInput(),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if not any(cleaned_data.get(field) for field in self.fields):
+            raise forms.ValidationError(_('At least one reason for the visit must be selected.'))
